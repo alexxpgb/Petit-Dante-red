d3530c3a771188bf96c28b0be6e9a1d5ca8617af
PG_alexandre <alexandre.petitghanem@ynov.com>
2023-09-18 11:26:02 +0200

    update mission 1

--- caracteristique/help.go
+++ caracteristique/help.go
@@ -35,3 +35,71 @@ func (p *Personnage) IsInSkill(s string) bool {
 	}
 	return false
 }
+
+func IsUpper(s string) bool {
+	nbs := len(s)
+	nb := 0
+	for _, c := range s {
+		for i := 65; i < 91; i++ {
+			if c == rune(i) {
+				nb += 1
+			}
+			if nb == nbs {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func IsLower(s string) bool {
+	nbs := len(s)
+	nb := 0
+	for _, c := range s {
+		for i := 97; i < 123; i++ {
+			if c == rune(i) {
+				nb += 1
+			}
+			if nb == nbs {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func IsAlpha(s string) bool {
+	for _, c := range s {
+		if c < 48 {
+			return false
+		}
+		if c > 122 {
+			return false
+		}
+	}
+	return true
+}
+
+func ToLower(s string) string {
+	var listf string
+	for _, c := range s {
+		if c > 64 && c < 91 {
+			listf = listf + string(c+32)
+		} else {
+			listf = listf + string(c)
+		}
+	}
+	return listf
+}
+
+func ToUpper(s string) string {
+	var listf string
+	for _, c := range s {
+		if c > 96 && c < 123 {
+			listf = listf + string(c-32)
+		} else {
+			listf = listf + string(c)
+		}
+	}
+	return listf
+}
