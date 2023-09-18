027f7e26809a1acea78cac4492a736606a9a4f3e
D_julien <julien.dante@ynov.com>
2023-09-18 11:26:44 +0200

    changes

--- caracteristique/help.go
+++ caracteristique/help.go
@@ -1,5 +1,7 @@
 package piscine
 
+import "fmt"
+
 func (p *Personnage) AddInventory(s string) {
 	for cle := range p.inventaire {
 		if cle == s {
@@ -34,4 +36,39 @@ func (p *Personnage) IsInSkill(s string) bool {
 		}
 	}
 	return false
+}
+
+func (p Personnage) LimitSpace() bool {
+	var count int
+	for cle := range p.inventaire {
+		if cle != "" {
+			count++
+		}
+	}
+	if count > 10 {
+		fmt.Println("Vous avez déjà 10 objets dans votre inventaire")
+		return false
+	}
+	return true
+}
+
+func (p *Personnage) UseObject(s string) {
+	for cle := range p.inventaire {
+		if cle == s {
+			p.inventaire[cle]--
+			if cle == "sucette" {
+				p.TakePot()
+			}
+			if cle == "totem" {
+				fmt.Println("Vous ne pouvez utiliser ce totem que si vous mourrez")
+			}
+			if cle == "Skill: go" {
+				fmt.Println("Vous pouvez maintenant apprendre le skill go dans Book Of Skill")
+				p.RemoveInventory("Skill: go")
+				p.BookOfSkills("go")
+			}
+		} else {
+			fmt.Println("Vous n'avez pas cette object dans votre inventaire")
+		}
+	}
 }
